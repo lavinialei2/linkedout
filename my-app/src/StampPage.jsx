268ea7f4471c3f7a78764d86e1837f0c772789a5
lavinialei2 <lavinialei@g.ucla.edu>
2025-02-02 00:10:38 -0800

    Back to map button

--- my-app/src/StampPage.jsx
+++ my-app/src/StampPage.jsx
@@ -1,16 +1,26 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from 'react';
+import { useNavigate } from 'react-router-dom'; 
 import './StampPage.css';
+import mapIcon from '/icons/mapicon.png';
 
 const StampPage = () => {
+  const navigate = useNavigate(); 
+
+  const handleMapClick = () => {
+    navigate('/map'); 
+  };
 
   return (
     <div className="app-container">
-        <img
-          src={'/icons/stamp.png'}
-          alt={`Stamp-page`}
-          className="slide-image"
-        />
+      <button className="map-button" onClick={handleMapClick}>
+        <img src={mapIcon} alt="Map Icon" className="map-icon" />
+      </button>
+      
+      <img
+        src={'/icons/stamp.png'}
+        alt="Stamp-page"
+        className="image"
+      />
     </div>
   );
 };
